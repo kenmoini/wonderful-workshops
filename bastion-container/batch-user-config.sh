6022561c00ed64236fc479c7d93470a6e83d1942
Ken Moini <ken@kenmoini.com>
2019-10-03 10:36:34 -0500

    more permissions because OpenShit

--- bastion-container/batch-user-config.sh
+++ bastion-container/batch-user-config.sh
@@ -5,6 +5,10 @@ for i in {0..100}; do
   sed "s/USER_HERE/student-user${i}/" /tmp/working/inprog_zshrc > /home/student-user${i}/.zshrc
   cp -r /root/.oh-my-zsh/ /home/student-user${i}/
   mkdir -p /home/student-user${i}/.local/share
+  mkdir -p /home/student-user${i}/.ssh
+  touch /home/student-user${i}/.ssh/{authorized_keys,config}
+  chmod 744 /home/student-user${i}/.ssh
+  chmod 644 /home/student-user${i}/.ssh/{authorized_keys,config}
   cp -r /root/.local/share/fonts/ /home/student-user${i}/.local/share/
   chown -R student-user${i}:student-user${i} /home/student-user${i}
 done
